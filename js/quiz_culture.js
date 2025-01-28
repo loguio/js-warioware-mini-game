079b8119fbf91729cf6921cf431a1562fc37c90b
BriceNogue <bricenogue@gmail.com>
2025-01-28 14:06:40 +0100

    Feat [ Quiz Culture] : Fonction

--- js/quiz_culture.js
+++ js/quiz_culture.js
@@ -2,120 +2,140 @@ const quizQuestions = [
     {
         id: 1,
         city: "Paris",
+        flag: "",
         options: ["France", "Italie", "Espagne", "Allemagne"],
         trueCountry: "France"
     },
     {
         id: 2,
         city: "Tokyo",
+        flag: "",
         options: ["Japon", "Chine", "Corée du Sud", "Thaïlande"],
         trueCountry: "Japon"
     },
     {
         id: 3,
         city: "New York",
+        flag: "",
         options: ["États-Unis", "Canada", "Royaume-Uni", "Australie"],
         trueCountry: "États-Unis"
     },
     {
         id: 4,
         city: "Berlin",
+        flag: "",
         options: ["Allemagne", "Pays-Bas", "Pologne", "Autriche"],
         trueCountry: "Allemagne"
     },
     {
         id: 5,
         city: "Madrid",
+        flag: "",
         options: ["Espagne", "Portugal", "Italie", "France"],
         trueCountry: "Espagne"
     },
     {
         id: 6,
         city: "Rome",
+        flag: "",
         options: ["Italie", "Grèce", "Égypte", "Turquie"],
         trueCountry: "Italie"
     },
     {
         id: 7,
         city: "Moscou",
+        flag: "",
         options: ["Russie", "Ukraine", "Biélorussie", "Kazakhstan"],
         trueCountry: "Russie"
     },
     {
         id: 8,
         city: "Pékin",
+        flag: "",
         options: ["Chine", "Japon", "Mongolie", "Vietnam"],
         trueCountry: "Chine"
     },
     {
         id: 9,
         city: "Canberra",
+        flag: "",
         options: ["Australie", "Nouvelle-Zélande", "Fidji", "Papouasie-Nouvelle-Guinée"],
         trueCountry: "Australie"
     },
     {
         id: 10,
         city: "Ottawa",
+        flag: "",
         options: ["Canada", "États-Unis", "Mexique", "Cuba"],
         trueCountry: "Canada"
     },
     {
         id: 11,
         city: "Lisbonne",
+        flag: "",
         options: ["Portugal", "Espagne", "Italie", "France"],
         trueCountry: "Portugal"
     },
     {
         id: 12,
         city: "Le Caire",
+        flag: "",
         options: ["Égypte", "Soudan", "Arabie Saoudite", "Jordanie"],
         trueCountry: "Égypte"
     },
     {
         id: 13,
         city: "Buenos Aires",
+        flag: "",
         options: ["Argentine", "Chili", "Brésil", "Paraguay"],
         trueCountry: "Argentine"
     },
     {
         id: 14,
         city: "Nairobi",
+        flag: "",
         options: ["Kenya", "Tanzanie", "Ouganda", "Rwanda"],
         trueCountry: "Kenya"
     },
     {
         id: 15,
         city: "Bangkok",
+        flag: "",
         options: ["Thaïlande", "Vietnam", "Malaisie", "Singapour"],
         trueCountry: "Thaïlande"
     },
     {
         id: 16,
         city: "Lima",
+        flag: "",
         options: ["Pérou", "Brésil", "Chili", "Argentine"],
         trueCountry: "Pérou"
     },
     {
         id: 17,
         city: "Séoul",
+        flag: "",
         options: ["Corée du Sud", "Corée du Nord", "Japon", "Mongolie"],
         trueCountry: "Corée du Sud"
     },
     {
         id: 18,
         city: "Helsinki",
+        flag: "",
         options: ["Finlande", "Suède", "Norvège", "Danemark"],
         trueCountry: "Finlande"
     },
     {
         id: 19,
         city: "Wellington",
+        flag: "",
         options: ["Nouvelle-Zélande", "Australie", "Fidji", "Samoa"],
         trueCountry: "Nouvelle-Zélande"
     },
     {
         id: 20,
         city: "Stockholm",
+        flag: "",
         options: ["Suède", "Norvège", "Finlande", "Danemark"],
         trueCountry: "Suède"
     }
