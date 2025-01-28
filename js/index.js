47c36b2e1d95c084efc6f0486c341b62425109f2
Nourisson Victor <victor.nourisson@supdevinci-edu.fr>
2025-01-28 17:09:12 +0100

    feat[index]: modification d'index.html et index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const games = [
     { name: 'escape', html: './html/escape.html', js: 'escape.js' },
     { name: 'tapeLeMot', html: './html/tapeLeMot.html', js: 'tapeLeMot.js' },
     { name: 'sigma', html: './html/sigma.html', js: 'sigma.js' },
+    { name: 'electricWire', html: './html/electricWire.html', js: 'electricWire.js' },
 ];
 
 // Affiche le titre comme un serpent qui bouge.
