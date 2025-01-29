09da978930f423baafe755006805b0615a0c50f2
Nathan <122289260+NathanRouleau@users.noreply.github.com>
2025-01-29 12:37:43 +0100

    NR-powerwash-v2 (#62)
    * feat[PowerWash]: create game
    
    * fix[powerwash]: reduction taille grille

--- js/index.js
+++ js/index.js
@@ -49,6 +49,7 @@ let remainingGames = [
   { name: "cible", html: "./html/cible.html", js: "cible.js" },
   { name: "memories", html: "./html/memories.html", js: "memories.js" },
   { name: "jeuHoraire", html: "./html/jeuHoraire.html", js: "jeuHoraire.js" },
+  { name: "powerwash", html: "./html/powerwash.html", js: "powerwash.js" },
   { name: "shadowBox", html: "./html/shadowBox.html", js: "shadowBox.js" },
   {
     name: "suiteNombre",
